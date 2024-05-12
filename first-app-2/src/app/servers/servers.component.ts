88e7a0dd9babe76d48d3ebf051b9133205375752
John Carlo Labanero <johncarlolabanero@gmail.com>
2024-05-12 19:31:54 +0800

    add passing data using event

--- first-app-2/src/app/servers/servers.component.ts
+++ first-app-2/src/app/servers/servers.component.ts
@@ -9,6 +9,7 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = "No server was created!";
+  serverName='';
   constructor() {
     setTimeout(()=>{
       this.allowNewServer = true;
@@ -17,5 +18,9 @@ export class ServersComponent implements OnInit {
   ngOnInit(){}
   onCreateServer(){
     this.serverCreationStatus = "Server was created!";
+  }
+  onUpdateServerName(event: Event){
+    console.log(event);
+    this.serverName = (<HTMLInputElement>event.target).value;
   }
 }
