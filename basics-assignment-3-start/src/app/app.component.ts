d7cb48021e0044ef8698c0d3dc6fedbebe9f1afe
John Carlo Labanero <johncarlolabanero@gmail.com>
2024-05-12 21:56:51 +0800

    finish ass-3

--- basics-assignment-3-start/src/app/app.component.ts
+++ basics-assignment-3-start/src/app/app.component.ts
@@ -3,7 +3,25 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  textToDisplay = 'Secret Password=tuna';
+  isDisplaying = true;
+  currNumber = 0;
+  items = [];
+  onDisplay() {
+    this.isDisplaying = !this.isDisplaying;
+    this.items.push(this.currNumber);
+    this.currNumber++;
+    // console.log(this.currNumber);
+  }
+  showDisplay() {
+    if (this.isDisplaying) return '';
+    return 'none';
+  }
+  getColor(item) {
+    if (item > 4) return 'white';
+    else return 'black';
+  }
 }
