a81d9f36b11353751cdec5b6fb4014c157ffd676
John Carlo Labanero <johncarlolabanero@gmail.com>
2024-05-12 18:30:39 +0800

    add success & warning alerts

---
+++ basics-assignment-1-start/src/app/warning-alert/warning-alert.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-warning-alert',
+  template: `<p class="bg-warning">warning-alert works!</p>`,
+  styleUrl: './warning-alert.component.css'
+})
+export class WarningAlertComponent {
+
+}
