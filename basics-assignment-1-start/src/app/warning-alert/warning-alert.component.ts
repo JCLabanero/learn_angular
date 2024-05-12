0bfde9e225e6bbef3c46e7c6c1fa553937566997
John Carlo Labanero <johncarlolabanero@gmail.com>
2024-05-12 18:33:12 +0800

    add inline css to warning

--- basics-assignment-1-start/src/app/warning-alert/warning-alert.component.ts
+++ basics-assignment-1-start/src/app/warning-alert/warning-alert.component.ts
@@ -2,9 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-warning-alert',
-  template: `<p class="bg-warning">warning-alert works!</p>`,
-  styleUrl: './warning-alert.component.css'
+  template: `<p>warning-alert works!</p>`,
+  styles: [
+    `
+      p {
+        background-color: yellow;
+        color: red;
+      }
+    `,
+  ],
 })
-export class WarningAlertComponent {
-
-}
+export class WarningAlertComponent {}
