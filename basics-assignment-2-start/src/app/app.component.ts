42ad65acc4f18e5f81cb07d0e98e8e5a05a882f8
John Carlo Labanero <johncarlolabanero@gmail.com>
2024-05-12 20:19:09 +0800

    update ass-2

--- basics-assignment-2-start/src/app/app.component.ts
+++ basics-assignment-2-start/src/app/app.component.ts
@@ -3,7 +3,21 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  hasString = false;
+  username = '';
+
+  onChange(event: Event) {
+    this.username = (<HTMLInputElement>event.target).value;
+    if (this.username != '') this.hasString = true;
+    else this.hasString = false;
+  }
+  setName() {
+    this.username = '';
+  }
+  isNameNotEmpty() {
+    return this.hasString;
+  }
 }
