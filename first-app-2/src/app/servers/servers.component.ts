ea9d2bbaea7c4c83ef44a2a2d164c26c0cbf14c9
John Carlo Labanero <johncarlolabanero@gmail.com>
2024-05-12 19:08:46 +0800

    add property binding not working

--- first-app-2/src/app/servers/servers.component.ts
+++ first-app-2/src/app/servers/servers.component.ts
@@ -1,13 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: '[app-servers]',
-  template: '<app-server></app-server>',
-  styleUrl: './servers.component.css'
+  // template: '<app-server></app-server>',
+  templateUrl: './servers.component.html',
+  styleUrl: './servers.component.css',
 })
-export class ServersComponent {
-
-  constructor() {}
+export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  constructor() {
+    setTimeout(()=>{
+      this.allowNewServer = true;
+    },2000);
+  }
   ngOnInit(){}
 
 }
