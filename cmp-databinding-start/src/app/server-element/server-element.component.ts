433fb6b12b003b50dd246b6d1eec26f5a55fd29f
John Carlo Labanero <johncarlolabanero@gmail.com>
2024-05-13 22:05:19 +0800

    update css encapsulation

--- cmp-databinding-start/src/app/server-element/server-element.component.ts
+++ cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -1,9 +1,11 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
   styleUrl: './server-element.component.css',
+  encapsulation: ViewEncapsulation.Emulated
+  // encapsulation: ViewEncapsulation.None // APPLY TO EVERYTHING (PARENT n its CHILDREN)
 })
 export class ServerElementComponent {
   @Input('srvElement') element: { type: string; name: string; content: string };
