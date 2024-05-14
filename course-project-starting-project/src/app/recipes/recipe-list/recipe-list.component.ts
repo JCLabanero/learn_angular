5a9f8ec4a456827a6b6b27e923bbd833ae211a1c
John Carlo Labanero <johncarlolabanero@gmail.com>
2024-05-14 12:37:36 +0800

    update passing data, event, n property binding

--- course-project-starting-project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ course-project-starting-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,6 +7,7 @@ import { Recipe } from '../recipe.model';
   styleUrl: './recipe-list.component.css',
 })
 export class RecipeListComponent {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe(
       'Test Recipe',
@@ -14,9 +15,12 @@ export class RecipeListComponent {
       'https://marketplace.canva.com/EAEzSj77_B4/2/0/1067w/canva-beige-cute-recipe-card-xDHAiGpfgKU.jpg'
     ),
     new Recipe(
-      'Test Recipe',
-      'The content',
-      'https://marketplace.canva.com/EAEzSj77_B4/2/0/1067w/canva-beige-cute-recipe-card-xDHAiGpfgKU.jpg'
+      'Test Recipe2',
+      'The content2',
+      'https://marketplace.canva.com/EAFNsV8XtFc/1/0/1067w/canva-white-modern-recipe-card-g0ij-n11PwM.jpg'
     ),
   ];
+  onRecipeSelected(recipe: Recipe){
+    this.recipeWasSelected.emit(recipe)
+  }
 }
