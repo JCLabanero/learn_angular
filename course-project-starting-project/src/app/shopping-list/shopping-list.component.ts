474e291d7594cc42499764c557de268db4a12af0
John Carlo Labanero <johncarlolabanero@gmail.com>
2024-05-16 13:25:08 +0800

    update emmiters into subscribe

--- course-project-starting-project/src/app/shopping-list/shopping-list.component.ts
+++ course-project-starting-project/src/app/shopping-list/shopping-list.component.ts
@@ -1,19 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Ingredient } from '../../shared/ingredient.model';
 import { ShoppingListService } from './shopping-list.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrl: './shopping-list.component.css',
 })
-export class ShoppingListComponent implements OnInit {
+export class ShoppingListComponent implements OnInit, OnDestroy {
   ingredients: Ingredient[];
+  private igChangeSub: Subscription;
   constructor(private shoppingService: ShoppingListService) {}
   ngOnInit() {
     this.ingredients = this.shoppingService.getIngredients();
-    this.shoppingService.ingredientChanged.subscribe((ingredients: Ingredient[])=>{
-      this.ingredients = ingredients;
-    });
+    this.igChangeSub = this.shoppingService.ingredientChanged.subscribe(
+      (ingredients: Ingredient[]) => {
+        this.ingredients = ingredients;
+      }
+    );
+  }
+  ngOnDestroy() {
+    this.igChangeSub.unsubscribe();
   }
 }
